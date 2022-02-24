4a2c58087519692f0f5161d735efcde5e6f7679c
Guilherme TJ <guips1998@gmail.com>
2022-02-24 17:49:10 -0300

    api de cursos

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,11 +14,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, UuidTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+
     protected $fillable = [
         'name',
         'email',
