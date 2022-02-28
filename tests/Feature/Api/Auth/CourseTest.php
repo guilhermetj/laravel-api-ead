95cb56fd6fa01c355e0055925cf444407c20f530
Guilherme TJ <guips1998@gmail.com>
2022-02-28 07:30:21 -0300

    teste api de cursos

---
+++ tests/Feature/Api/Auth/CourseTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use App\Models\Course;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\UtilsTrait;
+use Tests\TestCase;
+
+class CourseTest extends TestCase
+{
+   use UtilsTrait;
+
+    public function test_unauthenticated()
+    {
+
+        $token= $this->createTokenUser();
+
+        $response = $this->getJson('/courses');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_all_courses()
+    {
+
+        $response = $this->getJson('/courses', $this->defaultHeaders());
+
+        $response->assertStatus(200);
+    }
+    public function test_get_courses_total()
+    {
+
+        Course::factory()->count(10)->create();
+
+        $response = $this->getJson('/courses', $this->defaultHeaders());
+
+        $response->assertStatus(200)->assertJsonCount(10, 'data');
+    }
+    public function test_get_single_courses_unauthenticated()
+    {
+
+        Course::factory()->count(10)->create();
+
+        $response = $this->getJson('/courses/fake_id',);
+
+        $response->assertStatus(401);
+    }
+    public function test_get_single_courses_not_found()
+    {
+
+        Course::factory()->count(10)->create();
+
+        $response = $this->getJson('/courses/fake_id', $this->defaultHeaders());
+
+        $response->assertStatus(404);
+    }
+    public function test_get_single_courses()
+    {
+
+        $course = Course::factory()->create();
+
+        $response = $this->getJson("/courses/{$course->id}", $this->defaultHeaders());
+        $response->assertStatus(200);
+    }
+}
